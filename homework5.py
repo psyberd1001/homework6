678cb0e752a3c74bb3ac90fb68ebee1f3ab7c7a8
psyberd1001 <108468415+psyberd1001@users.noreply.github.com>
2024-03-18 19:22:48 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,12 @@
+my_list = ['ananas', 'banana', 'mango', 'cherry', 'avokado']
+print(my_list)
+print(my_list[0], my_list[-1])
+print(my_list[2:5])
+my_list[2] = "gomunkul"
+print(my_list)
+my_dict = {'Mango': 'Манго', 'Orange': 'Апельсин'}
+print(my_dict)
+print(my_dict.keys())
+my_dict['Mango'] = 'Не фрукт'
+my_dict.update({'Pahlava': 'Medovaya'})
+print(my_dict)
